c4de453d283da89b4f744274caff85d2c7ec1d53
piyushsakhare <piyushsakhare713@gmail.com>
2022-03-22 19:13:05 +0530

    api checks

--- src/components/context/auth/apiCalls.js
+++ src/components/context/auth/apiCalls.js
@@ -4,7 +4,7 @@ import { loginFail, loginStart, loginSuccess, logout } from "./AuthActions";
 export const login = async (user, dispatch) => {
     dispatch(loginStart())
     try{
-        const res = await axios.post("https://pretiosa-viventium-api.herokuapp.com/api/auth/login", user)
+        const res = await axios.post("auth/login", user)
         res.data.isAdmin && dispatch(loginSuccess(res.data))
     }catch(err){
         dispatch(loginFail())
