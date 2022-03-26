6db87b52fc67f5e64dab86874cef241394c1b490
piyushsakhare <piyushsakhare713@gmail.com>
2022-03-26 15:35:56 +0530

    destination changes

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -14,19 +14,22 @@ function Destinations() {
         deleteDestination(id, dispatch)
     }
 
+    console.log(destinations)
+
     return (
         <div>
             <h1 className="font-medium text-4xl" >Destinations List</h1>
             <div className="mt-8 grid grid-cols-1 divide-y" >
-                    {/* { destinations.map((destination, i) => {
-                        <div key={i} className="my-1 font-base flex justify-between" >
+                    { destinations.map((destination) => { 
+                        return (
+                        <div key={destination._id} className="my-1 font-base flex justify-between" >
                         <p>{destination.title}</p>
                         <div className="text-white" >
                             <button className="bg-blue-600 p-2 rounded" >Edit</button>
-                            <button className="bg-red-600 p-2 rounded ml-3" onClick={handleDelete(destination._id)} >Delete</button>
+                            <button className="bg-red-600 p-2 rounded ml-3" onClick={() => handleDelete(destination._id)} >Delete</button>
                         </div>
-                    </div>
-                    } ) } */}
+                    </div>)
+                    } ) }
                     
                     
                      
